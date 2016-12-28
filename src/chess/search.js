b5528de7c2da93cdf7e11ab46fc3233eac106a61
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-12-29 01:21:20 +0300

    feat(search): Extract depth

--- src/chess/search.js
+++ src/chess/search.js
@@ -1,27 +1,28 @@
-function search(game, depth) {
+function search(game, limit, depth) {
+    depth = depth || 0;
     var color = game.turn() == 'w' ? 1 : -1;
 
     if (game.game_over()) {
         if (game.in_draw()) {
-            return [0, game];
+            return [0, game, depth];
         } else if (game.in_checkmate()) {
-            return [Infinity * color, game];
+            return [Infinity * color, game, depth];
         }
 
         debugger;
         throw new Error('Unhandled end game');
     }
 
-    if (depth == 0) return [eval(game) * color, game];
+    if (depth == limit) return [eval(game) * color, game, depth];
 
     var moves = game.moves();
-    var best = [-Infinity, game];
+    var best = [-Infinity, game, depth];
 
     moves.forEach(function(move) {
         var newGame = game.clone();
         newGame.move(move);
 
-        var result = search(newGame, depth - 1);
+        var result = search(newGame, limit, depth + 1);
         result[0] = -1 * result[0];
         if (result[0] > best[0]) best = result;
     });
