4efd921f04b223b37ff572c93ba43034a4fd8942
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-01-01 19:13:09 +0300

    refactor(server): Prefer globals

--- src/server/app.js
+++ src/server/app.js
@@ -5,9 +5,9 @@ global._ = require('../client/js/lodash');
 global.eval2 = require('../chess/eval2');
 global.search = require('../chess/search');
 global.search2 = require('../chess/search2');
+global.Chess2 = require('../chess/chess2');
 
-const Chess = require('../chess/chess2');
-const game = new Chess();
+const game = new Chess2();
 
 console.time('search');
 console.log(search2(game, {depthLimitSoft: 4}));
