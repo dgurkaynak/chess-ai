2fe0364612b966b3e844190e94cfc143116bbe6d
Deniz Gurkaynak <dgurkaynak@gmail.com>
2017-01-01 23:11:55 +0300

    refactor(*): Clean-up

--- src/chess/eval.js
+++
@@ -1,9 +0,0 @@
-function eval(game) {
-    var factor = game.turn == 'w' ? 1 : -1;
-    var score = evalPieceValues(game) + evalPieceSquare(game);
-
-    // return score * factor;
-    return score;
-}
-
-if (typeof module !== 'undefined') module.exports = eval;
--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,5 @@
 global._ = require('../client/js/lodash');
-// global.evalPieceSquare = require('../chess/eval/piece-square');
-// global.evalPieceValues = require('../chess/eval/piece-values');
-// global.eval = require('../chess/eval');
 global.eval2 = require('../chess/eval2');
-global.search = require('../chess/search');
 global.search2 = require('../chess/search2');
 global.Chess2 = require('../chess/chess2');
 
